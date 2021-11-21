fba3b49022cba31ab27d8ccf28281f70796e8c17
Daniel Galán <danielgm.alcorcon@gmail.com>
2021-11-21 22:08:29 +0100

    v0.1

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 13, name: 'Castastor' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'Hombre Araña' },
+  { id: 17, name: 'Dinamo' },
+  { id: 18, name: 'Dr. Ingenius' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+  { id: 21, name: 'Relampago' },
+  { id: 22, name: 'El pistolero' }
+];
